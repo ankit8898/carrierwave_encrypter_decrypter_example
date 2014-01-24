c1e884a5fccae666b211ffaff3bfd0d1b09bf1a5
Ankit Gupta <ankit.gupta8898@gmail.com>
2014-01-24 10:28:24 -0500

    Sample App, carrierwave encrypter decrypter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ExampleCwEncDec::Application.config.secret_key_base = 'cfb22abdfea6dc4564a348b5781172601e633df3996ce41d0a131eaf73a12e8a1c80f6868299b5072c237b7801bb02aab29ca82294f574a3bd40d349490516db'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ExampleCwEncDec::Application.config.session_store :cookie_store, key: '_example_cw_enc_dec_session'
